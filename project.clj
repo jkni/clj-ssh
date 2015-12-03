1917ed90da8491834308ba8b702a55dd9deaea63
Joel Knighton <joel.knighton@datastax.com>
2015-12-03 10:43:30 -0600

    Prepare for Clojars upload

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (def agentproxy-version "0.0.7")
 
-(defproject jkni/clj-ssh "0.5.12-SNAPSHOT"
-  :description "Library for using SSH from clojure."
-  :url "https://github.com/hugoduncan/clj-ssh"
+(defproject org.clojars.jkni/clj-ssh "0.5.12-SNAPSHOT"
+  :description "Library for using SSH from clojure. Non-canonical fork integrating bug fixes that haven't been merged upstream"
+  :url "https://github.com/jkni/clj-ssh"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
